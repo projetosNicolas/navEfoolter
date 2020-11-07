0b4beb0b9d2988e54ca66b915c6df29f1f43198b
Nicolas <nicolassantoslira1@gmail.com>
2020-11-06 23:50:01 -0300

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+/*
+const idWindows = window.innerWidth;
+
+if(idWindows > 700)
+{
+    alert("Bem vindo ao The perfector");
+}else{
+    alert("Bem vindo ao The perfector. Baixe nosso aplicativo para melhor usabilidade");
+}
+*/
